87f3c43053579a9de604e5fb56b66df456dbe0fe
Carl Christian Solli <carl_solli@hotmail.com>
2018-10-13 14:12:02 -0700

    A simple flashcard lab

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -20,6 +20,12 @@ class ViewController: UIViewController {
 
     
     @IBAction func didTapOnFlashcard(_ sender: Any) {
+        if frontLabel.isHidden {
+            frontLabel.isHidden = false
+        }
+        else {
+            frontLabel.isHidden = true
+        }
     }
     
 }
